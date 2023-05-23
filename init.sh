e6ad25d0266974a05bee129b4ea66eb515297a79
Vadym <ia03.prokhorchuk.vadym@gmail.com>
2023-05-23 05:16:01 +0300

    Fix Terraform

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 #! /bin/sh
 
 sudo apt-get update
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-chkconfig docker on
+sudo apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
 
-sudo socker pull vadymprokhorchuk/html-server-image
+
+sudo docker pull vadymprokhorchuk/html-server-image
 sudo docker pull containrrr/watchtower
 sudo docker run -d --name=site -p 80:80 vadymprokhorchuk/html-server-image
 sudo docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 10
